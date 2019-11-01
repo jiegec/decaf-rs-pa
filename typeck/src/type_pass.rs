d6c2e01f199d060e910aaf6876fa0d0896a1da96
Jiajie Chen <noc@jiegec.ac.cn>
2019-11-01 15:24:29 +0800

    Fix testcase for calling a var

--- typeck/src/type_pass.rs
+++ typeck/src/type_pass.rs
@@ -367,7 +367,7 @@ impl<'a> TypePass<'a> {
       }
       None => {
         let cur = self.cur_class.unwrap();
-        if let Some(sym) = cur.lookup(v.name) {
+        if let Some((sym, _owner)) = self.scopes.lookup(v.name) {
           self.check_normal_call(v, c, Ty::mk_obj(cur), sym, loc)
         } else {
           self.issue(loc, NoSuchField { name: v.name, owner: Ty::mk_obj(cur) })
